fab3720aea8f02fef742c1bfee4486c99dbd424e
Veljko Arsic <veljko.skate96@gmail.com>
2019-06-11 15:24:29 +0200

    commit

--- app/src/main/java/vex/firma/moja/veljkoarsicandroid/Zadatak8Activity.java
+++ app/src/main/java/vex/firma/moja/veljkoarsicandroid/Zadatak8Activity.java
@@ -1,4 +1,14 @@
 package vex.firma.moja.veljkoarsicandroid;
 
-class Zadatak8Activity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Zadatak8Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zadatak8);
+    }
+
 }
